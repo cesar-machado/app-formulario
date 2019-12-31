749de576f106ceaf5aa6592c7a908f5e89de8ec4
cesar-machado <cesarmach19@gmail.com>
2019-12-31 00:07:15 -0300

    Coloquei o componente do formulário, arrumei o css e coloquei rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component'
+import { FormularioComponent } from './formulario/formulario.component'
+  import { from } from 'rxjs';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'formulario', component: FormularioComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
